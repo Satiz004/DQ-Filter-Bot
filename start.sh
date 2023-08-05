cc896a75b6bd8ab653958679dff2331060c2d4cf
Mr.Rolex <139060034+yogi444@users.noreply.github.com>
2023-08-05 10:30:55 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/yogi444/DQ-Filter-Bot.git /DQ-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  git clone $UPSTREAM_REPO /DQ-Filter-Bot
 fi
-cd /DQTheFileDonor
+cd /DQ-Filter-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
